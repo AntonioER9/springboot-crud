f82b8e037c931997f548c4b49e292cf14a78e365
Antonio Espinoza <a0e0bhj@walmart.com>
2025-01-31 17:25:49 -0300

    add Product entity and repository for CRUD operations

---
+++ src/main/java/com/antonio/course/springboot/app/springboot_crud/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.antonio.course.springboot.app.springboot_crud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.antonio.course.springboot.app.springboot_crud.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}
