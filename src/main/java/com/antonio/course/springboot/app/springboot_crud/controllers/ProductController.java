a7632444387d0cc40d23c7e17eff85a560c12ed9
Antonio Espinoza <a0e0bhj@walmart.com>
2025-02-10 17:57:19 -0300

    add validation handling in ProductController and introduce messages.properties

--- src/main/java/com/antonio/course/springboot/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/antonio/course/springboot/app/springboot_crud/controllers/ProductController.java
@@ -1,58 +1,68 @@
 package com.antonio.course.springboot.app.springboot_crud.controllers;
 
+import com.antonio.course.springboot.app.springboot_crud.entities.Product;
+import com.antonio.course.springboot.app.springboot_crud.services.ProductService;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.antonio.course.springboot.app.springboot_crud.entities.Product;
-import com.antonio.course.springboot.app.springboot_crud.services.ProductService;
-
-import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
   @Autowired
-  private ProductService productService;
+  private ProductService service;
+
+  // @Autowired
+  // private ProductValidation valdation;
 
   @GetMapping
   public List<Product> list() {
-    return productService.findAll();
+    return service.findAll();
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<?> view(@PathVariable Long id) {
-    Optional<Product> productOptional = productService.findById(id);
+    Optional<Product> productOptional = service.findById(id);
     if (productOptional.isPresent()) {
       return ResponseEntity.ok(productOptional.orElseThrow());
-    } else {
-      return ResponseEntity.notFound().build();
     }
+    return ResponseEntity.notFound().build();
   }
 
   @PostMapping
-  public ResponseEntity<Product> create(@Valid @RequestBody Product product) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
+  public ResponseEntity<?> create(@Valid @RequestBody Product product, BindingResult result) {
+    // valdation.validate(product, result);
+    if (result.hasFieldErrors()) {
+      return validation(result);
+    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<Product> update(@PathVariable Long id, @Valid @RequestBody Product product) {
-
-    Optional<Product> productOptional = productService.update(id, product);
+  public ResponseEntity<?> update(@Valid @RequestBody Product product, BindingResult result, @PathVariable Long id) {
+    // valdation.validate(product, result);
+    if (result.hasFieldErrors()) {
+      return validation(result);
+    }
+    Optional<Product> productOptional = service.update(id, product);
     if (productOptional.isPresent()) {
       return ResponseEntity.status(HttpStatus.CREATED).body(productOptional.orElseThrow());
     }
@@ -61,11 +71,19 @@ public class ProductController {
 
   @DeleteMapping("/{id}")
   public ResponseEntity<?> delete(@PathVariable Long id) {
-    Optional<Product> productOptional = productService.delete(id);
+    Optional<Product> productOptional = service.delete(id);
     if (productOptional.isPresent()) {
       return ResponseEntity.ok(productOptional.orElseThrow());
-    } else {
-      return ResponseEntity.notFound().build();
     }
+    return ResponseEntity.notFound().build();
+  }
+
+  private ResponseEntity<?> validation(BindingResult result) {
+    Map<String, String> errors = new HashMap<>();
+
+    result.getFieldErrors().forEach(err -> {
+      errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+    });
+    return ResponseEntity.badRequest().body(errors);
   }
 }
